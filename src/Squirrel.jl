363e721e9bd69448959f3ff3670ec0245991a333
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2022-08-24 18:03:42 +0100

    initial implementation

--- src/Squirrel.jl
+++ src/Squirrel.jl
@@ -1,5 +1,29 @@
 module Squirrel
 
-# Write your package code here.
+using JLSO
+
+macro squirrel(arg::Symbol) # single item to squirrel away
+    local argstr = string(arg)
+
+    return quote
+        $JLSO.save("squirrel.jlso", Symbol($argstr) => $(esc(arg)))
+        println("using JLSO")
+        println("loaded = JLSO.load(\"squirrel.jlso\")")
+        println($argstr, " = loaded[:", $argstr, "]")
+    end
+end
+
+macro squirrel(ex::Expr) # multiple items to squirrel
+    (ex.head == :tuple) || throw(ArgumentError("squirrel macro accepts a single symbol or a tuple"))
+    local argstrs = string.(ex.args)
+    local pairs = [Symbol(s) => a for (s, a) in zip(ex.args, argstrs)]
+
+    return quote
+        $JLSO.save("squirrel.jlso", $pairs...)
+        println("using JLSO")
+        println("loaded = JLSO.load(\"squirrel.jlso\")")
+        println(join($argstrs, ", "), " = loaded[:", join($argstrs, "], loaded[:"), "]")
+    end
+end
 
 end
