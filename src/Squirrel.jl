2cbdd396295fff073c95b4e0a6735b73945550d6
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2022-08-25 10:41:16 +0100

    save the file to ~/.julia/squirrel/

--- src/Squirrel.jl
+++ src/Squirrel.jl
@@ -2,13 +2,27 @@ module Squirrel
 
 using JLSO
 
+export @squirrel
+
+# create multiple files
+const SFILE = expanduser("~/.julia/squirrel/squirrel.jlso")
+
+function __init__()
+    mkpath(dirname(SFILE))
+
+    # TODO: remove files more than a month old or something
+    return nothing
+end
+
+# TODO: expand the syntax for multiple symbols as well
+
 macro squirrel(arg::Symbol) # single item to squirrel away
     local argstr = string(arg)
 
     return quote
-        $JLSO.save("squirrel.jlso", Symbol($argstr) => $(esc(arg)))
+        $JLSO.save($SFILE, Symbol($argstr) => $(esc(arg)))
         println("using JLSO")
-        println("loaded = JLSO.load(\"squirrel.jlso\")")
+        println("loaded = JLSO.load(\"", $SFILE, "\")")
         println($argstr, " = loaded[:", $argstr, "]")
     end
 end
@@ -19,9 +33,9 @@ macro squirrel(ex::Expr) # multiple items to squirrel
     local pairs = [Symbol(s) => a for (s, a) in zip(ex.args, argstrs)]
 
     return quote
-        $JLSO.save("squirrel.jlso", $pairs...)
+        $JLSO.save($SFILE, $pairs...)
         println("using JLSO")
-        println("loaded = JLSO.load(\"squirrel.jlso\")")
+        println("loaded = JLSO.load(\"", $SFILE,"\")")
         println(join($argstrs, ", "), " = loaded[:", join($argstrs, "], loaded[:"), "]")
     end
 end
